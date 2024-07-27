039637470855f64778a3ceb7a079a19ce06018f8
MarvelStacks Dev1 <119876237+MarvelStacksDev1@users.noreply.github.com>
2024-07-27 21:53:46 +0530

    Setup the Nav Bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,29 @@
 import Link from "next/link";
 import React from "react";
+import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
-  return <></>;
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/isses" },
+  ];
+  return (
+    <nav className="flex space-x-5 border-b px-3 h-14 items-center mb-2 bg-cyan-200">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-5">
+        {links.map((link) => (
+          <li
+            key={link.href}
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+          >
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
