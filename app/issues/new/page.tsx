48033ca83acbee2eb385e4d76b9fecc6dcc5561e
MarvelStacks Dev1 <119876237+MarvelStacksDev1@users.noreply.github.com>
2024-07-28 02:02:00 +0530

    Add a markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,4 +1,7 @@
-import { TextField, TextArea, Button } from "@radix-ui/themes";
+"use client";
+import { TextField, Button } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 import React from "react";
 
 const NewIssuePage = () => {
@@ -6,7 +9,7 @@ const NewIssuePage = () => {
     <div className="max-w-xl space-y-3">
       <h1>Add New Issue</h1>
       <TextField.Root placeholder="Title"></TextField.Root>
-      <TextArea placeholder="Description" />
+      <SimpleMDE placeholder="Description" />
       <Button>Submit New Issue</Button>
     </div>
   );
