618b7b8dbc069688007a60e8c06c5402a0f8611d
MarvelStacks Dev1 <119876237+MarvelStacksDev1@users.noreply.github.com>
2024-07-27 22:08:01 +0530

    Setup classnames

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,14 +1,20 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/isses" },
+    { label: "Issues", href: "/issues" },
   ];
   return (
-    <nav className="flex space-x-5 border-b px-3 h-14 items-center mb-2 bg-cyan-200">
+    <nav className="flex space-x-5 border-b px-3 h-14 items-center mb-2 bg-orange-300">
       <Link href="/">
         <AiFillBug />
       </Link>
@@ -16,7 +22,11 @@ const NavBar = () => {
         {links.map((link) => (
           <li
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
           >
             <Link href={link.href}>{link.label}</Link>
           </li>
@@ -27,3 +37,7 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
+// classnames -- > {`${
+//     link.href === currentPath ? "text-zinc-900" : "text-zinc-500"
+//   } hover:text-zinc-800 transition-colors`}
