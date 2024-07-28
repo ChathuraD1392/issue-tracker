3abaaf8cea205333bd4eac4a459c3f636e6a8921
MarvelStacks Dev1 <119876237+MarvelStacksDev1@users.noreply.github.com>
2024-07-28 21:59:49 +0530

    create new user

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
 import { Theme } from "@radix-ui/themes";
+import { useState } from "react";
 
 const inter = Inter({
   subsets: ["latin"],
