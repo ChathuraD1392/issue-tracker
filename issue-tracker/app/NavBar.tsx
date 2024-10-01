d6dc24955f2f50ebcf980e7c6badf3973e67f45e
ChathuraD1392 <chathuradilshan1392@gmail.com>
2024-10-01 09:33:43 +0530

    Create issue model

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -1,21 +1,32 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { FaBug } from "react-icons/fa";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
   return (
     <nav className="flex space-x-5 px-5 border-b mb-5 h-12 items-center">
-      <Link href="/">{<FaBug />}</Link>
-      <ul className="flex space-x-5">
+      <Link href="/">
+        <FaBug />
+      </Link>
+      <ul className="flex space-x-5 font-semibold">
         {links.map((link, index) => (
           <li key={index}>
             <Link
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
-              href={link.label}
+              className={classnames({
+                "text-zinc-900": currentPath === link.href,
+                "text-zinc-500": currentPath != link.href,
+                "hover:text-zinc-900 transition-colors": true,
+              })}
+              href={link.href}
             >
               {link.label}
             </Link>
